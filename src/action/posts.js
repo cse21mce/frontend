a5d72522e6e4a41a833a510b2cb35acc905c6f38
Rahul Kumar <rahulkumar703@outlook.com>
2024-08-11 20:04:08 +0530

    fixed deployment error

--- src/action/posts.js
+++ src/action/posts.js
@@ -18,8 +18,8 @@ export const getPosts = async () => {
 export const getPost = async (title) => {
     try {
 
-        const pressReleases = await PressRelease.findOne({ title });
-        return pressReleases;
+        const pressRelease = await PressRelease.findOne({ title });
+        return pressRelease;
 
     } catch (error) {
         return null
@@ -28,13 +28,13 @@ export const getPost = async (title) => {
 
 export const getRecentPosts = async (title) => {
     try {
-        const pressReleases = await PressRelease.find()
+        const recentReleases = await PressRelease.find()
             .sort({ date_posted: -1 })  // Sort by date_posted in descending order
             .limit(5);                   // Limit the result to 5 documents
-        return pressReleases.map(p => ({ ...p._doc, _id: p._doc._id.toString() }));
+        return recentReleases.map(p => ({ ...p._doc, _id: p._doc._id.toString() }));
 
     } catch (error) {
-        return null
+        return []
     }
 }
 
